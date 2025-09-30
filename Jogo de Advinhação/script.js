c6eb6b73e47df68c213384c8c2e1a97230f235d0
Vitor Emanuel <Vitor00180066100@gmail.com>
2025-09-30 12:09:38 -0300

    Add files via upload

--- Jogo de Advinhação/script.js
+++ Jogo de Advinhação/script.js
@@ -13,7 +13,7 @@ const chancesRestantesElement = document.querySelector('#chancesRestantes')
 
 let listaDePalpites = []; // array para guardar os palpites que o jogador já deu
 
-function iniciar() {
+function iniciarJogo() {
     // Gerar um novo número secreto (entre 1 e 100)
     // math.random() gera um número entre 0 (inclusive) e 1 (exclusive)
     // multiplicamos por 100 para ter entre 0 e 99.99..
@@ -21,4 +21,28 @@ function iniciar() {
     // math.floor() arredonda para o menor inteiro, então teremos entre 1 e 99
     numeroSecreto = Math.floor(Math.random() * 100) + 1;
     console.log('numero Secreto (apenas para teste):', numeroSecreto); // adjuda no debug!
+    
+    // Resetar o contador de chances
+    contadorDeChances = 1;
+    chancesRestantesElement.textContent = MAX_CHANCES; //mostra o total de chances
+
+    //limpar as mensagens anteriores
+    mensagemElement.textContent = '';
+    palpitesAnterioresElement.textContent = '';
+    dicaElement.textContent = '';
+    listaDePalpites = [];
+
+
+
+    mensagemElement.style.color = '#f0f0f0'; // cor padrão do texto
+    dicaElement.style.color = '#ffeb3b'; // cor padrao da dica
+
+    // reativar o campo de palpite e o botao de enviar
+    campoPalpite.disabled = false;
+    botaoEnviarPalpite.disabled = false;
+    campoPalpite.value = ''; // limpa o campo de entrada
+    campoPalpite.focus(); // coloca o cursor no campo para o usuário digitar
+
+    // esconder botao de reiniciar
+    botaoReiniciar.classList.add('hidden');
 }
